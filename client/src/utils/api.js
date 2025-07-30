80db182f90904de88a019aca309300c295018654
Syed Kafeel Shah <100509974+KafeelShah@users.noreply.github.com>
2025-07-31 03:19:36 +0500

    URL Updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+  baseURL: "https://real-estate-silk-xi-63.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
